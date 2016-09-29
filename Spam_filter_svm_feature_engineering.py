b4d540fd569a46c6518695b3d77a51bc26ed0153
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2016-09-29 13:08:12 -0400

    Add files via upload

--- Spam_filter_svm_feature_engineering.py
+++ Spam_filter_svm_feature_engineering.py
@@ -16,6 +16,7 @@ Data created: September 27, 2016
 # import packages
 import numpy as np
 import pandas as pd
+from scipy import sparse
 from nltk.corpus import stopwords
 from pandas import read_csv
 from sklearn.feature_extraction.text import HashingVectorizer
@@ -29,6 +30,7 @@ from sklearn.linear_model import SGDClassifier
 
 # sms file from computer hard drive - no need to load data
 file_location = r'C:\Users\Windows\Dropbox\AllStuff\Spam_filter\Data\SMSSpamCollection'
+sms = pd.read_csv(file_location, sep='\t', names=["label", "message"])
 
 ##############################
 # 2. PREPROCESSING TEXT DATA #
@@ -41,7 +43,6 @@ def tokenizer(text):
         
 # Add character and word numbers in text msg to check predictive accuracy via
 # SVM algorithm.
-sms['charnum'] = sms['message'].map(lambda text: len(text))
 sms['wordnum'] = sms['message'].map(lambda text: len(text.split()))
 
 # Add the number of words in all caps
@@ -49,6 +50,7 @@ def capwords(text):
     text_words = text.split()  
     text_words_upper_case = [word.upper() for word in text_words]
     return len(set(text_words) & set(text_words_upper_case))
+        
 
 
 ###################################
@@ -60,14 +62,18 @@ print(nrows)
 
 # generator function that reads in one document at a time
 def stream_docs(path):
-    with open(path) as csv:
+    with open(path, 'r', encoding='utf-8') as csv:
         for line in csv:
             text, label = line[4:-1], line[:4]
             if (label == 'ham\t'):
                 label = 'ham'
-            yield text, label
+            text = list(text) # convert string into characters
             
-print(next(stream_docs(path=file_location)))
+            if (text[0] == '\t'):
+                del text[0] # remove tab character
+            
+            text = ''.join(text) # join characters back to text string
+            yield text, label
 
 # take a document stream from stream_docs and return a particular number of 
 # documents specified by the size parameter
@@ -84,11 +90,29 @@ def get_minibatch(doc_stream, size):
 
 doc_stream = stream_docs(path=file_location)
 
-print(get_minibatch(doc_stream, size=5))                    
+#print(get_minibatch(doc_stream, size=5))                    
 
 #####################################################################
 # 4. TRAINING MODELS VIA ONLINE ALGORITHMS AND OUT-OF-CORE LEARNING #
 #####################################################################
+
+# add features to text data set and return as a vector
+def addfeatures(data):
+    # additional feature    
+    capword_feat = np.array([capwords(lines) for lines in data])
+    capword_feat.shape = (len(data), 1)
+    capword_feat = sparse.csc_matrix(capword_feat)
+    char_feat = np.array([len(lines) for lines in data])
+    char_feat.shape = (len(data), 1)
+    char_feat = sparse.csc_matrix(char_feat)
+    data = vect.transform(data)
+    data = sparse.hstack([char_feat, capword_feat, data])    
+    return data
+    
+def addnofeatures(data):
+    # additional feature    
+    data = vect.transform(data)
+    return data    
 
 # vectorize text via HashingVectorizer
 vect = HashingVectorizer(decode_error='ignore',
@@ -99,26 +123,16 @@ clf = SGDClassifier(loss='hinge', random_state=1, n_iter=1)
 
 classes = np.array(['ham', 'spam'])
 
-for _ in range(1):
-    X_train, y_train = get_minibatch(doc_stream, size=3)
+for _ in range(40):
+    X_train, y_train = get_minibatch(doc_stream, size=100)
     if not X_train:
-        break
-     # additional feature
-    print("***************")    
-    print(X_train)   
-#    sms = X_train.value.map(lambda text: capwords(text))
-#    capnum = sms.values
-#    capnum.shape = (len(sms), 1)
-    X_train = vect.transform(X_train)
-    
-    # additional feature
-#    X_train = np.hstack((capnum, X_train))
-    
-    print(X_train.shape)
+        break        
+    X_train = addnofeatures(X_train)    
     clf.partial_fit(X_train, y_train, classes=classes)
     
 X_test, y_test = get_minibatch(doc_stream, size=(nrows - 5 - 4000))
-X_test = vect.transform(X_test)
+X_test = addnofeatures(X_test)    
+
 print('Accuracy: %.3f' % (clf.score(X_test, y_test)*100))    
 
 
